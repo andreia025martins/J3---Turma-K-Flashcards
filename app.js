496899f9e8638e9a0e578975baeccc41869a8bab
Andréia <andreia.martins17@escola.pr.gov.br>
2024-11-07 19:14:34 -0300

    ajuste

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ function criaCartao(categoria, pergunta, resposta){
     cartao.className = 'cartao'
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-                    <h3>GITHUB</h3>
-                    <div class="cartao__conteudo__pergunta">
-                        <p>O que é GITHUB?</p>
-                    </div>
-                    <div class="cartao__conteudo__resposta">
-                        <p>O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.</p>
-                    </div>
-                </div>
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+    <p>${resposta}</p>
+    </div>
+    </div>
     `
+    container.appendChild(cartao)
 }
