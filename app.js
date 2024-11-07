883c7d1075873be41c4f1783ba58d9bb9e7410ba
Andréia <andreia.martins17@escola.pr.gov.br>
2024-11-07 19:08:15 -0300

    Ajuste reunião 6

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 function criaCartao(categoria, pergunta, resposta){
-    console.log(categoria, pergunta, resposta);
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+                    <h3>GITHUB</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>O que é GITHUB?</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.</p>
+                    </div>
+                </div>
+    `
 }
